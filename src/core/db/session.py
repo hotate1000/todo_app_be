e4635578919894db44155f014dbd2c0516ffa84d
hotate1000 <ooo.336.sss@gmail.com>
2024-10-13 01:05:52 +0900

    現状コミット

---
+++ src/core/db/session.py
@@ -0,0 +1,37 @@
+from contextvars import ContextVar
+from ..setting import get_setting
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession, async_scoped_session
+
+
+setting = get_setting()
+
+
+session_context = ContextVar("session_context", default=None)
+
+
+def create_engine_and_session_factory(database_url: str):
+    # create_async_engine(): 非同期データベースエンジンを作成し、データベースとの接続を非同期で管理。
+    engine = create_async_engine(
+        database_url,
+        echo=True,
+        future=True
+    )
+
+    # async_sessionmaker(): 非同期セッションを作成し、セッションを通じて非同期でデータベース操作。
+    session_factory = async_sessionmaker(
+        bind=engine,
+        expire_on_commit=False,
+        class_=AsyncSession
+    )
+
+    return session_factory
+
+
+def setup():
+    db_url = f"mysql+aiomysql://{setting.db_user}:{setting.db_password}@{setting.db_host}/{setting.db_name}?charset=utf8mb4"
+    session_factory = create_engine_and_session_factory(db_url)
+    session = async_scoped_session(session_factory, scopefunc=session_context.get)
+    return session
+
+
+session = setup()
--- src/core/setting.py
+++ src/core/setting.py
@@ -9,7 +9,14 @@ class Setting:
     app_name: str = os.getenv("APP_NAME")
     app_description: str = os.getenv("APP_DESCRIPTION")
     app_version: str = os.getenv("APP_VERSION")
+    db_user: str = ""
+    db_password: str = ""
+    db_host: str = ""
+    db_name: str = ""
 
+
+# @lru_cacheについて関数の結果をメモリに保存するデコレータ。
+# 関数が同じ引数で何度も呼び出された際に、計算結果を再利用してパフォーマンスを向上させる。計算コストが高い関数や、頻繁に同じ結果を返す関数で効果的。
 @lru_cache
 def get_setting():
     try:
